d8f1f7994c1ffd2cefa3f46cccfa220d4defcad6
Anastasiia Yermakova <yermak1037@gmail.com>
2021-01-17 01:35:57 +0200

    add sound ticking clock

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const refs = {
   second: document.querySelector('.second-hand'),
   minute: document.querySelector('.minute-hand'),
   hour: document.querySelector('.hour-hand'),
+  audio: document.querySelector('#audio'),
+  btn: document.querySelector('.btn'),
 };
 
 const getSeconds = date => (date.getSeconds() / 60) * 360 - 90;
@@ -12,12 +14,25 @@ const getMinutes = date =>
 const getHours = date =>
   (date.getHours() / 12) * 360 + (date.getMinutes() / 60) * 30 - 90;
 
+const playaudioSound = () => {
+  refs.btn.classList.toggle('playing');
+};
+
+refs.btn.addEventListener('click', playaudioSound);
+
 const setDate = () => {
   const date = new Date();
 
   refs.second.style.transform = `rotate(${getSeconds(date)}deg)`;
   refs.minute.style.transform = `rotate(${getMinutes(date)}deg)`;
   refs.hour.style.transform = `rotate(${getHours(date)}deg)`;
+
+  if (refs.btn.classList.contains('playing')) {
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    audio.pause();
+  }
 };
 
 setInterval(setDate, 1000);
