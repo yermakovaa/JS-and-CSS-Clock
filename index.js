fef10a3393e981eac4dbeec6b474673a16676ab3
Anastasiia Yermakova <yermak1037@gmail.com>
2021-01-16 16:15:03 +0200

    feat: add clock

---
+++ index.js
@@ -0,0 +1,25 @@
+const refs = {
+  second: document.querySelector('.second-hand'),
+  min: document.querySelector('.min-hand'),
+  hour: document.querySelector('.hour-hand'),
+};
+
+const getSeconds = date => (date.getSeconds() / 60) * 360 + 90;
+
+const getMinutes = date =>
+  (date.getMinutes() / 60) * 360 + (date.getSeconds() / 60) * 6 + 90;
+
+const getHours = date =>
+  (date.getHours() / 12) * 360 + (date.getMinutes() / 60) * 30 + 90;
+
+const setDate = () => {
+  const date = new Date();
+
+  refs.second.style.transform = `rotate(${getSeconds(date)}deg)`;
+  refs.min.style.transform = `rotate(${getMinutes(date)}deg)`;
+  refs.hour.style.transform = `rotate(${getHours(date)}deg)`;
+};
+
+setInterval(setDate, 1000);
+
+setDate();
